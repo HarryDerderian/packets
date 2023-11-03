e4495d20bbbef91fc13bc9f1e3d27dd20e12768c
HarryDerderian <hderderian@student.cccd.edu>
2023-11-02 23:50:25 -0700

    small change

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import scapy.all as scapy
 
-# My first question: What is a packet?
-# Google shows: A packet is data sent over networks.
-# Some key aspects of this data: Source IP, Dest IP
-
-
-
 if __name__ == "__main__" :
-        
-    # Create an ICMP packet
+    # Targeted IP... aka ourself, don't want any legal trouble.
     LOCAL_HOST_IP = "127.0.0.1"
-    packet = scapy.IP( dst = LOCAL_HOST_IP) / scapy.ICMP()
-                      # dst (the ip we want to send the packet to)
     
-    
-    
-    # Send the packet
+    # FLAGS (commands our packet will send out)
+    req_2_flag = {"SYN" : "S"}
+    target_port = 80 # (HTTP)
+    # Transmission Control Protocol
+    TCP = scapy.TCP(dport = target_port, flags = req_2_flag["SYN"])
+    packet = scapy.IP( dst = LOCAL_HOST_IP) / TCP
     try :
-        scapy.send(packet)
+        # Send out the packet, wait for a response.
+        response = scapy.sr1(packet)
+        print(str(response))
+        print("END PROGRAM")
     except :
         print("MUST RUN AS ADMINISTRATOR")
         print("Program requires to be ran as admin.")
