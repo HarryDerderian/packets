af4059290a8f48d5dadef2546302120f0d7029c3
HarryDerderian <hderderian@student.cccd.edu>
2023-11-03 00:07:10 -0700

    commit

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ if __name__ == "__main__" :
     LOCAL_HOST_IP = "127.0.0.1"
     
     # FLAGS (commands our packet will send out)
-    req_2_flag = {"SYN" : "S"}
-    target_port = 80 # (HTTP)
+    cmd_2_flag = {"SYN" : "S"}
+    target_port = 21 # (SSH)
     # Transmission Control Protocol
-    TCP = scapy.TCP(dport = target_port, flags = req_2_flag["SYN"])
-    packet = scapy.IP( dst = LOCAL_HOST_IP) / TCP
+    TCP = scapy.TCP(dport = target_port, flags = cmd_2_flag["SYN"])
+    TARGET_IP = scapy.IP(dst = LOCAL_HOST_IP)
+    # Merge the above with '/' this will create a complete packet.
+    packet = scapy.IP(dst = LOCAL_HOST_IP) / TCP
     try :
         # Send out the packet, wait for a response.
         response = scapy.sr1(packet)
